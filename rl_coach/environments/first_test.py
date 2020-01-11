601303b864a855e5ba75bdf233ad25dd21fa46ed
lukas.madzik <luker@madzik.sk>
2020-01-11 11:31:03 +0100

    pred updateom

--- rl_coach/environments/first_test.py
+++ rl_coach/environments/first_test.py
@@ -19,25 +19,22 @@ class Player:
 
 
 class FacEnv:
-    map = None
     target = None
-    player = None
     wrongs = None
 
     def __init__(self):
         self.wrongs = 0
-        self.player = Player()
         # self.target = {'x': 2, 'y': 2}
-        self.target = {'x': np.random.randint(0, 49), 'y': np.random.randint(0, 49)}
+        self.target = {'x': np.random.randint(1, 14), 'y': np.random.randint(1, 14)}
         print(self.target['x'], self.target['y'])
-        self.observation = np.zeros((50, 50, 1), dtype=int)
+        self.observation = np.zeros((16, 16, 1), dtype=int)
         # setting target
-        self.observation[self.target['x']][self.target['y']] = 1
+        self.observation[self.target['x']][self.target['y']][0] = 1
 
 
 class TestCls(Environment):
     env = None
-    reward_limit = 0
+    new_reward = 0
 
     def __init__(self, level: LevelSelection, seed: int, frame_skip: int, human_control: bool,
                  custom_reward_threshold: Union[int, float], visualization_parameters: VisualizationParameters,
@@ -45,8 +42,8 @@ class TestCls(Environment):
         super().__init__(level, seed, frame_skip, human_control, custom_reward_threshold, visualization_parameters,
                          **kwargs)
         self.state_space = StateSpace({
-            # "observation": VectorObservationSpace(shape=4)
-            "observation": PlanarMapsObservationSpace(shape=np.array([50, 50, 1]), low=0, high=1)
+            # "observation": VectorObservationSpace(shape=2)
+            "observation": PlanarMapsObservationSpace(shape=np.array([16, 16, 1]), low=0, high=1)
             # "observation": TensorObservationSpace(shape=np.array([2, 2]), low=0, high=1)
             # "player": VectorObservationSpace(shape=2),
             # "map": PlanarMapsObservationSpace(shape=np.array([84, 84, 1]), low=0, high=2),
@@ -56,11 +53,9 @@ class TestCls(Environment):
         self.action_space = DiscreteActionSpace(num_actions=4,
                                                 descriptions={"0": "up", "1": "down", "2": "left", "3": "right"})
         self.env = FacEnv()
-        self.reward_limit = -np.power(np.sum(np.arange(1 + np.abs(self.env.target['x']) + np.abs(self.env.target['y'])))/2, 2)
 
     def _take_action(self, action_idx: ActionType) -> None:
-        # print(self.env.player.y, self.env.player.x)
-        self.env.observation[self.env.target['x']][self.env.target['y']] = 0
+        self.env.observation[self.env.target['x']][self.env.target['y']][0] = 0
         if action_idx == 0:
             self.env.target['y'] += 1
         if action_idx == 1:
@@ -69,22 +64,42 @@ class TestCls(Environment):
             self.env.target['x'] -= 1
         if action_idx == 3:
             self.env.target['x'] += 1
-        self.env.observation[self.env.target['x']][self.env.target['y']] = 1
+        # self.new_reward = -(np.abs(7 - self.env.target['x']) + np.abs(7 - self.env.target['y']))
+        if self.env.target['x'] == 0:
+            self.done = True
+            self.reward = -10000
+            self.env.target['x'] = 1
+            # self.new_reward *= 5
+        if self.env.target['y'] == 0:
+            self.done = True
+            self.reward = -10000
+            self.env.target['y'] = 1
+            # self.new_reward *= 5
+        if self.env.target['x'] == 15:
+            self.done = True
+            self.reward = -10000
+            self.env.target['x'] = 14
+            # self.new_reward *= 5
+        if self.env.target['y'] == 15:
+            self.done = True
+            self.reward = -10000
+            self.env.target['y'] = 14
+            # self.new_reward *= 5
+        self.env.observation[self.env.target['x']][self.env.target['y']][0] = 1
+        print("Moved to: ", self.env.target['x'], ", ", self.env.target['y'])
 
     def _update_state(self) -> None:
-        new_reward = -np.power(
-                    np.abs(24 - self.env.target['x']) + np.abs(24 - self.env.target['y']), 2)
-
-        if self.reward > new_reward:
-            self.env.wrongs += 1
-        if self.env.target['x'] == 24 and self.env.target['y'] == 24:
-            new_reward = 2000
-        self.reward = new_reward
-        # self.goal = np.array([self.env.target['x'], self.env.target['y'], self.env.target['x'], self.env.target['y']])
-        # self.done = ((self.env.player.x == self.env.target['x']) and (self.env.player.y == self.env.target['y'])) or (np.abs(self.env.player.x) > 80 or np.abs(self.env.player.y) > 80)
-        self.done = (self.env.target['x'] == 24 and self.env.target['y'] == 24) or self.env.target['x'] <= 0 or self.env.target['y'] <= 0 or self.env.target['x'] >= 49 or self.env.target['y'] >= 49 or self.current_episode_steps_counter > 100
+        if not self.done:
+            if self.reward > self.new_reward:
+                self.env.wrongs += 1
+            if self.env.target['x'] == 7 and self.env.target['y'] == 7:
+                self.new_reward = 10000
+            self.reward = -(np.abs(7 - self.env.target['x']) + np.abs(7 - self.env.target['y']))
+            # self.goal = np.array([self.env.target['x'], self.env.target['y'], self.env.target['x'], self.env.target['y']])
+            # self.done = ((self.env.player.x == self.env.target['x']) and (self.env.player.y == self.env.target['y'])) or (np.abs(self.env.player.x) > 80 or np.abs(self.env.player.y) > 80)
+            self.done = (self.env.target['x'] == 7 and self.env.target['y'] == 7) or self.env.target['x'] <= 0 or self.env.target['y'] <= 0 or self.env.target['x'] >= 15 or self.env.target['y'] >= 15 or self.current_episode_steps_counter > 125
         self.state = {
-            # "observation": np.array([self.env.player.x, self.env.player.y, self.env.target['x'], self.env.target['y']])
+            # "observation": np.array([self.env.target['x'], self.env.target['y']])
             "observation": self.env.observation
             # "observation": np.array([[self.env.player.x, self.env.player.y], [self.env.target['x'], self.env.target['y']]])
             # "player": np.array([self.env.player.x, self.env.player.y]),
@@ -92,20 +107,20 @@ class TestCls(Environment):
         }
 
     def _restart_environment_episode(self, force_environment_reset=False) -> None:
-        if (24 == self.env.target['x']) and (24 == self.env.target['y']):
+        if (7 == self.env.target['x']) and (7 == self.env.target['y']):
             print("TARGET")
-        if self.env.target['x'] > 49 or self.env.target['y'] > 49 or self.env.target['x'] < 1 or self.env.target['y'] < 1:
+        if self.env.target['x'] > 15 or self.env.target['y'] > 15 or self.env.target['x'] < 1 or self.env.target['y'] < 1:
             print("LOST")
-        self.env.player = Player()
-        # self.env.target = {'x': np.random.randint(0, 49), 'y': np.random.randint(0, 49)}
-        self.env.target = {'x': 34, 'y': 12}
+        self.env.target = {'x': np.random.randint(1, 14), 'y': np.random.randint(1, 14)}
+        # self.env.target = {'x': 34, 'y': 12}
+        self.env.observation = np.zeros((16, 16, 1), dtype=int)
+        self.env.observation[self.env.target['x']][self.env.target['y']][0] = 1
         print("Wrongs: ", self.env.wrongs)
         print(self.env.target['x'], self.env.target['y'])
         self.env.wrongs = 0
-        self.reward_limit = -np.power(np.sum(np.arange(1 + np.abs(self.env.target['x']) + np.abs(self.env.target['y'])))/2, 2)
 
     def get_rendered_image(self):
-        image_map = np.copy(self.env.observation).astype(float)
+        image_map = np.copy(self.env.observation[:, :, 0]).astype(float)
         image_map[image_map == 0] = 255
         image_map[image_map == 1] = 170
         # new_image = np.zeros((50, 50, 3))
--- rl_coach/presets/first_test_DQN.py
+++ rl_coach/presets/first_test_DQN.py
@@ -3,23 +3,27 @@ from rl_coach.agents.dqn_agent import DQNAgentParameters
 from rl_coach.architectures.embedder_parameters import InputEmbedderParameters
 from rl_coach.architectures.middleware_parameters import FCMiddlewareParameters
 from rl_coach.graph_managers.graph_manager import SimpleSchedule, SimpleScheduleWithoutEvaluation
-from rl_coach.core_types import EnvironmentSteps, TrainingSteps, AlwaysDumpFilter
+from rl_coach.core_types import EnvironmentSteps, TrainingSteps, AlwaysDumpFilter, EnvironmentEpisodes
 from rl_coach.base_parameters import VisualizationParameters, PresetValidationParameters, MiddlewareScheme, \
     EmbedderScheme
 from rl_coach.graph_managers.basic_rl_graph_manager import BasicRLGraphManager
 from rl_coach.environments.first_test import ControlSuiteEnvironmentParameters
-
+from rl_coach.schedules import LinearSchedule
 
 agent_params = DQNAgentParameters()
-agent_params.network_wrappers['main'].middleware_parameters = FCMiddlewareParameters(scheme=MiddlewareScheme.Medium)
+agent_params.algorithm.num_steps_between_copying_online_weights_to_target = EnvironmentSteps(100)
+agent_params.exploration.evaluation_epsilon = 0
+agent_params.exploration.epsilon_schedule = LinearSchedule(1, 0.1, 100000)
+# agent_params.network_wrappers['main'].middleware_parameters = FCMiddlewareParameters(scheme=MiddlewareScheme.Medium)
 # agent_params.network_wrappers['main'].batch_size = 1
-# agent_params.network_wrappers['main'].input_embedders_parameters = {
-#     "observation": InputEmbedderParameters(scheme=EmbedderScheme.Empty)
-# }
-# rename the input embedder key from 'observation' to 'measurements'
+agent_params.network_wrappers['main'].should_get_softmax_probabilities = True
+agent_params.network_wrappers['main'].input_embedders_parameters = {
+    "observation": InputEmbedderParameters(scheme=EmbedderScheme.Empty)
+}
 # agent_params.network_wrappers['main'].input_embedders_parameters['measurements'] = agent_params.network_wrappers['main'].input_embedders_parameters.pop('observation')
 schedule_params = SimpleSchedule()
-schedule_params.heatup_steps = EnvironmentSteps(10000)
+schedule_params.steps_between_evaluation_periods = EnvironmentEpisodes(50)
+schedule_params.heatup_steps = EnvironmentSteps(0)
 preset_validation_params = PresetValidationParameters()
 # preset_validation_params.test = True
 # preset_validation_params.min_reward_threshold = 20
